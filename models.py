2b20f9011b370ce0cbce1fa67356bdfaf1a709b4
aeggert17 <aprileggert17@gmail.com>
2026-05-03 14:12:21 -0400

    Update models.py

--- models.py
+++ models.py
@@ -1,13 +1,12 @@
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
+from flask_login import UserMixin
 
 db = SQLAlchemy()
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(256), nullable=False)
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    password = db.Column(db.String(200), nullable=False)
 @property
 def is_active(self):
     return True
